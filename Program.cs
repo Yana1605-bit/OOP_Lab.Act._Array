38b747d0e312d01a48a2c5b2844d2f0e1d33c65f
Diana Oxina <oxinadiana@gmail.com>
2025-08-09 20:04:49 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Act._02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] fruit = new string[5];
+            int apple = 0;
+
+            for (int i = 0; i < fruit.Length; i++)
+            {
+                Console.Write($"Enter a fruit {i + 1}: ");
+                fruit[i] = Console.ReadLine().ToLower();
+            }
+
+            foreach (string color in fruit)
+            {
+                if (color == "apple")
+                {
+                    apple++;
+                }
+            }
+
+            Console.WriteLine($"\nNumbers of 'apple' entered: {apple}");
+        }
+    }
+}
