71dbea306ce0ae9c2df2bf1d2eadecce92384c88
Diana Oxina <oxinadiana@gmail.com>
2025-08-09 21:16:40 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,37 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Act._04
+namespace Act._06
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string[] names = new string[3];
+            int[] celsius = new int[5];
 
-            for (int i = 0; i < names.Length; i++)
+            for (int i = 0; i < celsius.Length; i++)
             {
-                Console.Write($"Enter 3 names {i + 1}: ");
-                names[i] = Console.ReadLine();
+                Console.Write($"Enter temperature in Celsius {i + 1}: ");
+                celsius[i] = int.Parse(Console.ReadLine());
+
+                float fahrenheit = (celsius[i] * 9 / 5) + 32;
+                Console.WriteLine($"Temperature in Fahrenheit: {fahrenheit}");
+                Console.WriteLine("\n");
             }
-            Console.WriteLine("\n");
 
-            foreach (string name in names)
+            foreach (int temp in celsius)
             {
-                if (!string.IsNullOrEmpty(name) && name.Length >= 2)
+                if (temp < 15)
                 {
-                    char fChar = name[0];
-                    char lChar = name[name.Length - 1];
-                    char upFirst = char.ToUpper(fChar);
-                    char upLast = char.ToUpper(lChar);
-
-                    Console.WriteLine($"First character: {fChar}");
-                    Console.WriteLine($"Last character: {lChar}");
-
-                    Console.WriteLine($"First in uppercase: {upFirst}");
-                    Console.WriteLine($"Last in uppercase: {upLast}");
-
-                    Console.WriteLine();
+                    Console.WriteLine("\nCold");
+                }
+                else if (temp < 15 && temp > 30)
+                {
+                    Console.WriteLine("\nWarm");
+                }
+                else
+                {
+                    Console.WriteLine("\nHot");
                 }
             }
         }
